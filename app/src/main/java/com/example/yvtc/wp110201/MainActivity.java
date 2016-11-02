a36c8e16a292a841842cfd214df454a5095e7a79
aa <aa@aa.bb>
2016-11-02 20:44:45 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yvtc/wp110201/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.yvtc.wp110201;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Button btn = new Button(MainActivity.this);
+        //btn.setText("Click!!");
+        //Button btn2 = new Button(MainActivity.this);
+        //btn2.setText("Click 2222");
+        //TextView tv = new TextView(MainActivity.this);
+        //tv.setText("hihihi");
+        //LinearLayout layout = (LinearLayout) findViewById(R.id.activity_main);
+        //layout.addView(btn);
+        //layout.addView(btn2);
+        //layout.addView(tv);
+    }
+}
